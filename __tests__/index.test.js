0235c547b0b21ac3775982338e10e815b81f85d1
zaycevas <zaycevas@loniir.ru>
2021-06-18 13:44:32 +0300

    проверка тестов

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import io from 'socket.io-client';
 import testingLibrary from '@testing-library/react';
-import ChatForm from '../src/Components/Chat/ChatForm.jsx';
+import init from '../src/index';
 
 const { render, screen } = testingLibrary;
+const socket = io();
 
-test('chat form', () => {
-  render(<ChatForm />);
+beforeEach(async () => {
+  const vdom = await init(socket);
+  render(vdom);
+});
+
+test('test of test', () => {
   screen.debug();
 });
