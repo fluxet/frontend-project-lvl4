2ff4260a9d127be90a363a1eafd02826f7e342ae
zaycevas <zaycevas@loniir.ru>
2021-06-21 18:09:16 +0300

    проверка тестов v7

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,28 +4,15 @@ import RegeneratorRuntime from 'regenerator-runtime';
 
 import io from 'socket.io-client';
 import { render, screen } from '@testing-library/react';
+import init from '../src/init.js';
 
 const socket = io();
 
 beforeEach(async () => {
-  const init = await (await import('../src/init.js')).default;
-  console.log('init: ', init);
-
   const vdom = await init(socket);
   render(vdom);
 });
 
-test('test of test', () => {
+test('', () => {
   screen.debug();
 });
-
-// import init from '../src/init.js'; // приложение импортируется как зависимость
-
-// beforeEach(async () => { // выполнится перед каждым тест-кейсом
-//   const vdom = await init(socket.socketClient); // ожидаемый интерфейс вашего приложения
-//   render(vdom);
-// });
-
-// test('', () => {
-//   screen.debug();
-// });
