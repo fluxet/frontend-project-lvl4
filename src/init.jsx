4562da7c84086e4f88fd9f26205a0248c448d1e9
fluxet <fluxet@mail.ru>
2021-08-22 00:53:12 +0300

    вынесение методов сокета в init

--- src/init.jsx
+++ src/init.jsx
@@ -1,17 +1,48 @@
 // @ts-check
 import React from 'react';
 import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import i18next from 'i18next';
+import io from 'socket.io-client';
 import ru from './locales/ru.js';
 
 import debug from '../lib/logger.js';
-import store from './store.js';
+// import store from './store.js';
+import channels, {
+  addChannel, removeChannel, renameChannel,
+} from './stateSlices/channelsSlice.js';
+import messages, { addMessage } from './stateSlices/messagesSlice.js';
+import modalType from './stateSlices/modalTypeSlice.js';
 import App from './App.jsx';
 import { ContextWs } from './context.js';
 
 const log = debug('init');
 log.enabled = true;
+
+const reducer = combineReducers({
+  channels, messages, modalType,
+});
+
+const store = configureStore({
+  reducer,
+});
+
+const socket = io();
+
+socket.on('newMessage', (message) => {
+  store.dispatch((addMessage(message)));
+});
+socket.on('newChannel', (channel) => {
+  store.dispatch(addChannel(channel));
+});
+socket.on('removeChannel', (channel) => {
+  store.dispatch(removeChannel(channel));
+});
+socket.on('renameChannel', (channel) => {
+  store.dispatch(renameChannel(channel));
+});
 
 const WsProvider = ({ wsClient, children }) => (
   <ContextWs.Provider value={{ wsClient }}>{children}</ContextWs.Provider>
--- src/store.js
+++ src/store.js
@@ -1,34 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
-import io from 'socket.io-client';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { combineReducers } from 'redux';
 
-import channels, {
-  addChannel, removeChannel, renameChannel,
-} from './stateSlices/channelsSlice.js';
-import messages, { addMessage } from './stateSlices/messagesSlice.js';
-import modalType from './stateSlices/modalTypeSlice.js';
+// import channels, {
+//   addChannel, removeChannel, renameChannel,
+// } from './stateSlices/channelsSlice.js';
+// import messages, { addMessage } from './stateSlices/messagesSlice.js';
+// import modalType from './stateSlices/modalTypeSlice.js';
 
-const reducer = combineReducers({
-  channels, messages, modalType,
-});
+// const reducer = combineReducers({
+//   channels, messages, modalType,
+// });
 
-const store = configureStore({
-  reducer,
-});
+// const store = configureStore({
+//   reducer,
+// });
 
-const socket = io();
-
-socket.on('newMessage', (message) => {
-  store.dispatch((addMessage(message)));
-});
-socket.on('newChannel', (channel) => {
-  store.dispatch(addChannel(channel));
-});
-socket.on('removeChannel', (channel) => {
-  store.dispatch(removeChannel(channel));
-});
-socket.on('renameChannel', (channel) => {
-  store.dispatch(renameChannel(channel));
-});
-
-export default store;
+// export default store;
