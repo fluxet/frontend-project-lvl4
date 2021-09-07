f64086bfcc6c4483b4cfb8bb41c0ca8ec909a75a
zaycevas <zaycevas@loniir.ru>
2021-09-07 12:52:01 +0300

    удаление combineReducers

--- src/store.js
+++ src/store.js
@@ -1,17 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
 
 import channels from './stateSlices/channelsSlice.js';
 import messages from './stateSlices/messagesSlice.js';
 import errors from './stateSlices/errorSlice.js';
 import modal from './stateSlices/modalSlice.js';
 
-const reducer = combineReducers({
-  channels, messages, modal, errors,
-});
-
 const store = configureStore({
-  reducer,
+  reducer: {
+    channels, messages, modal, errors,
+  },
 });
 
 export default store;
